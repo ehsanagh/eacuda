c04c6f460e4f107ee1181d039622fadd8d211494
Hosein D.Farahani <hosein71@yahoo.com>
2015-07-19 15:18:11 +0430

    Update main.js

--- HTML/js/main.js
+++ HTML/js/main.js
@@ -59,34 +59,11 @@ $(document).ready(function(){
         $(this).find('img').removeClass('shake shake-slow');
     });
 
+    $('.toup').click(function() {
 
-	jQuery(document).ready(function($){
-		// browser window scroll (in pixels) after which the "back to top" link is shown
-		var offset = 300,
-			//browser window scroll (in pixels) after which the "back to top" link opacity is reduced
-			offset_opacity = 1200,
-			//duration of the top scrolling animation (in ms)
-			scroll_top_duration = 700,
-			//grab the "back to top" link
-			$back_to_top = $('.toup');
-	
-		//hide or show the "back to top" link
-		$(window).scroll(function(){
-			( $(this).scrollTop() > offset ) ? $back_to_top.addClass('toup-visible') : $back_to_top.removeClass('toup-visible');
-		});
-		
-		//smooth scroll to top
-		$back_to_top.on('click', function(event){
-			event.preventDefault();
-			$('body,html').animate({
-				scrollTop: 0 ,
-			 	}, scroll_top_duration
-			);
-		});
-	
-	});
-    
-    
+        $.fn.fullpage.moveTo('home');
+    });
+
     var AnimationArray = [
         '#homeSection .slogan','#homeSection .btn',
         '#aboutSection .pageTitle','#aboutSection .pageDescription','#aboutSection .serviceItem',
@@ -158,6 +135,12 @@ $(document).ready(function(){
             darkMenus.forEach(function(item) {
                 console.log(item);
                 if (anchorLink == item) { menu.addClass('darkStyle'); removeit = false;}
+                if (anchorLink != 'home') {
+                	$('.toup').addClass('toup-visible');
+                }
+                else {
+                	$('.toup').removeClass('toup-visible');
+                }
             });
 
             removeit ? menu.removeClass('darkStyle') : null;
@@ -284,5 +267,3 @@ function animateThese(itemArray) {
     }
 
 }
-
-
